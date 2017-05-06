54f6508d30cc5f78b04c945443e3882b004cc2fb
Caio Lopes <caiodesalopes@gmail.com>
2017-05-06 20:01:20 -0300

    Add normalized stacked bar chart

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import { StackedBar } from 'components/Viz';
+
 class Dashboard extends Component {
 
   render() {
     return (
       <div className="animated fadeIn">
-        Hello World
+        <StackedBar />
       </div>
     )
   }
