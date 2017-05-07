cfe58a200d3cb4ff58797d4fe594b0844c8ba066
Maglethong Spirr <maglethongspirr@gmail.com>
2017-05-07 12:01:38 -0300

    Added Partenrs cards

--- src/views/ParticipacaoSocietaria/ParticipacaoSocietaria.js
+++ src/views/ParticipacaoSocietaria/ParticipacaoSocietaria.js
@@ -1,12 +1,38 @@
 import React, {Component} from 'react';
-import {Card, CardBlock, CardHeader, Col, Row} from 'reactstrap';
+import {Card, CardBlock, CardHeader} from 'reactstrap';
 import report from '../../report';
 
-import { StackedBar, MirrorBar } from 'components/Viz';
+import {StackedBar, MirrorBar} from 'components/Viz';
 
 const socios = report.controleSocietario.socios.map(s => {
   s.spc = s.spc === null ? 0 : s.spc.valor;
   return s;
+});
+
+const CardList = React.createClass({
+  render: function () {
+    return ( <div>
+      { this.props.data.map(function (socio) {
+        return (
+            <Card style={{margin: 10}}>
+              <CardBlock>
+                <div className="h1 text-muted text-right mb-2">
+                  <i className="icon-people" />
+                </div>
+                <h5 style={{display: "inline"}}>{socio.nome}</h5>
+                <div className="float-right">{socio.cpf}{socio.cnpj}</div>
+                <p className="mb-0"><small className="text-muted text-uppercase font-weight-bold">SPC</small></p>
+                <div className="progress progress-xs mt-1 mb-0">
+                  <div className="progress-bar bg-danger" role="progressbar" style={{width:socio.spc}} aria-valuenow={socio.spc} aria-valuemin="0" aria-valuemax="1" />
+                </div>
+              </CardBlock>
+            </Card>
+        )
+      })
+      }
+      </div>
+    )
+  }
 });
 
 class ParticipacaoSocietaria extends Component {
@@ -17,18 +43,25 @@ class ParticipacaoSocietaria extends Component {
   render() {
     return (
         <div className="animated fadeIn">
-          <Row>
-            <Col xl="12">
+          <div className="row">
+            <div className="col-sm-12">
               <Card>
                 <CardHeader>
                   Participação Societaria
                 </CardHeader>
-                <CardBlock>
-                  <MirrorBar id="mirror-bar-1" data={socios}/>
-                </CardBlock>
+                <div className="row">
+                  <div className="col-sm-8">
+                    <CardBlock>
+                      <MirrorBar id="mirror-bar-1" data={socios}/>
+                    </CardBlock>
+                  </div>
+                  <div className="col-sm-4">
+                    <CardList data={socios}/>
+                  </div>
+                </div>
               </Card>
-            </Col>
-          </Row>
+            </div>
+          </div>
         </div>
 
     )
